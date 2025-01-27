3da8d5fce6dd8d0d940fa592ccb19f7f2cf756a5
Thiago Arruda <thiago.arruda.09@hotmail.com>
2025-01-27 14:51:18 -0300

    adiciona padding aos labels

--- agenda/js/pages/cadastrar.js
+++ agenda/js/pages/cadastrar.js
@@ -15,7 +15,7 @@ function cadastrar() {
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
 </svg>
-              <label for="Nome">Nome</label>
+              <label for="Nome" class="p-2">Nome</label>
               
               <input
            
@@ -30,7 +30,7 @@ function cadastrar() {
   <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
 </svg>
 
-              <label for="Telefone">Telefone</label>
+              <label for="Telefone" class="p-2">Telefone</label>
               
               <input
               
@@ -46,7 +46,7 @@ function cadastrar() {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet" viewBox="0 0 16 16">
   <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a2 2 0 0 1-1-.268M1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1"/>
 </svg>
-              <label for="email">Email (opcional)</label>
+              <label for="email" class="p-2">Email (opcional)</label>
               <input
              
                 type="email"
